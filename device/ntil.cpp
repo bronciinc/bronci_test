f2e397d465435a9ba7881f612e6fb50c1661da7e
lance-cl-lu <lance.cl.lu@gmail.com>
2026-05-27 19:06:06 +0800

    Add dummy frame

--- device/ntil.cpp
+++ device/ntil.cpp
@@ -415,8 +415,8 @@ static void jobSendingVideo(uint32_t tid)
 
   ts_frame--;
   // Dummy Frame to trigger the sending of previous frames in case some frames are still in queue of SDK
-  // result = NTIL_VideoPutInData( lineID, pbuff_p, size_p, FRAME_TYPE_P, ts_frame, frame_id, gop_id, fps, bps, H264);
-  // mylog::message(" -NTIL_VideoPutInData() Dummy Frame I {} ", frame_id);
+  result = NTIL_VideoPutInData( lineID, pbuff_p, size_p, FRAME_TYPE_P, ts_frame, frame_id, gop_id, fps, bps, H264);
+  mylog::message(" -NTIL_VideoPutInData() Dummy Frame I {} ", frame_id);
 
   delete [] pbuff_i;
   delete [] pbuff_p;
